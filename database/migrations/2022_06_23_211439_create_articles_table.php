ff1350ecf511fba8a110c4a8b6a28544194fd02b
Alexandr Stecuk <sandrostecuk@gmail.com>
2022-06-24 00:39:59 +0300

    add(*): controller, model, nigration

---
+++ database/migrations/2022_06_23_211439_create_articles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id()->autoincrement();
+            $table->string('article_author', 100)->nullable();
+            $table->text('article_title')->nullable();
+            $table->text('article_content')->nullable();
+            $table->text('article_image_names')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
